c5cd1ab92b64c5477e9ae712a92c0d3e5ca603ab
Yuval Hager <yhager@yhager.com>
2016-05-27 15:42:00 -0700

    add python dependency detection

---
+++ python.cmake
@@ -0,0 +1,36 @@
+function(python_pip_dependencies _deps)
+  if (NOT DEFINED CPACK_GENERATOR)
+    sonar_cpack_generator(CPACK_GENERATOR)
+  endif()
+  if (${CPACK_GENERATOR} STREQUAL "RPM")
+    foreach(python_pkg pip devel)
+      foreach(python_ver python python27 python26 python34u)
+        set(package ${python_ver}-${python_pkg})
+        execute_process(
+          COMMAND rpm "--quiet" "-q" ${package}
+          RESULT_VARIABLE rv)
+        if (${rv} EQUAL 0)
+          execute_process(
+            COMMAND rpm "-q" "--queryformat" "%{NAME} = %{EPOCHNUM}:%{VERSION}-%{RELEASE}" ${package}
+            OUTPUT_VARIABLE dep)
+          list(APPEND deplist ${dep})
+        endif()
+      endforeach()
+    endforeach()
+  elseif(${CPACK_GENERATOR} STREQUAL "DEB")
+    foreach(python_pkg pip dev)
+      foreach(python_ver python python2.7 python2.6 python3 python3.4)
+        set(package ${python_ver}-${python_pkg})
+        execute_process(
+          COMMAND dpkg-query "--show" "--showformat" "\${Package}(=\${Version})" ${package}
+          OUTPUT_VARIABLE dep)
+        list(APPEND deplist ${dep})
+      endforeach()
+    endforeach()
+  endif()
+  if (deplist)
+    string(REPLACE ";" ", " deps "${deplist}")
+  endif()
+  message(STATUS "Python dependencies: ${deps}")
+  set(${_deps} "${deps]" PARENT_SCOPE)
+endfunction()
