8dae99b0fe621dc1390ff7625364a120d9a2ac68
Yuval Hager <yhager@yhager.com>
2018-07-04 16:52:45 -0700

    compile sqlite with fts5 support

--- SonarExternal.cmake
+++ SonarExternal.cmake
@@ -414,6 +414,7 @@ function(build_sqlite3)
     "cmake_minimum_required(VERSION 3.8)\n"
     "project(sqlite LANGUAGES C)\n"
     "add_library(sqlite3 sqlite3.c)\n"
+    "target_compile_definitions(sqlite3 PRIVATE SQLITE_ENABLE_FTS5)\n"
     "install(TARGETS sqlite3 DESTINATION lib)\n"
     "install(FILES sqlite3.h DESTINATION include)\n")
   message(STATUS "Building sqlite3 from ${SQLITE3_URL}")
