110ae4998ffa3f123843f05e42208a19de0b46f4
Yuval Hager <yhager@yhager.com>
2018-11-09 12:55:20 -0800

    upgrade to catch-2.4.2 by default

--- SonarExternal.cmake
+++ SonarExternal.cmake
@@ -1121,7 +1121,7 @@ function(build_catch2)
   cmake_parse_arguments(CATCH2 "" VERSION "" ${ARGN})
   message(STATUS "Using headers from catch2-${CATCH2_VERSION}")
   if(NOT CATCH2_VERSION)
-    set(CATCH2_VERSION 2.2.3)
+    set(CATCH2_VERSION 2.4.2)
   endif()
   ExternalProject_Add(catch2
     URL https://github.com/catchorg/Catch2/archive/v${CATCH2_VERSION}.tar.gz
