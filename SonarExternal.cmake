47ceea7c4c6e61de650eed26dccf952352dbce66
Mikaela Allan <mikaela@jsonar.com>
2020-01-10 12:26:58 -0800

    enable sqlite3 memory management

--- SonarExternal.cmake
+++ SonarExternal.cmake
@@ -561,8 +561,15 @@ function(build_sqlite3)
   file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/sqlite3.cmake
     "cmake_minimum_required(VERSION 3.8)\n"
     "project(sqlite LANGUAGES C)\n"
+
     "add_library(sqlite3 sqlite3.c)\n"
-    "target_compile_definitions(sqlite3 PRIVATE SQLITE_ENABLE_FTS5 SQLITE_ENABLE_RTREE)\n"
+
+    "target_compile_definitions(sqlite3 PRIVATE\n"
+    "  SQLITE_ENABLE_FTS5\n"
+    "  SQLITE_ENABLE_RTREE\n"
+    "  SQLITE_ENABLE_MEMORY_MANAGEMENT\n"
+    ")\n"
+
     "install(TARGETS sqlite3 DESTINATION lib)\n"
     "install(FILES sqlite3.h sqlite3ext.h DESTINATION include)\n")
   message(STATUS "Building sqlite3 from ${SQLITE3_URL}")
