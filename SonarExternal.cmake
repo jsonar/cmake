1fe1dae53855de9e89b1506a4bf3fd68459fa61f
Arthur Castro <arthur@jsonar.com>
2020-11-18 18:01:14 -0800

    remove pcre valgrind

--- SonarExternal.cmake
+++ SonarExternal.cmake
@@ -724,7 +724,6 @@ function(build_pcre)
     CONFIGURE_COMMAND <SOURCE_DIR>/configure
       CC=${CMAKE_C_COMPILER_LAUNCHER}\ ${CMAKE_C_COMPILER}
       --enable-jit
-      --enable-valgrind
       --enable-unicode-properties
       --enable-utf
       --prefix <INSTALL_DIR>
