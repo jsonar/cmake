7d4e8dd96518fdb592840ae6a65ae4b784684628
Christopher Papke <chris.papke@imperva.com>
2025-01-02 08:39:30 -0800

    update boost source

--- SonarExternal.cmake
+++ SonarExternal.cmake
@@ -1596,7 +1596,7 @@ function(build_boost)
     set(download_step SOURCE_DIR ${BOOST_SOURCE_DIR})
   else()
     set(download_step URL
-      https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION_UNDERSCORES}.tar.bz2
+      https://archives.boost.io/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION_UNDERSCORES}.tar.bz2
       https://sourceforge.net/projects/boost/files/boost/${BOOST_VERSION}/boost_${BOOST_VERSION_UNDERSCORES}.tar.bz2/download)
   endif()
   foreach(component ${BOOST_COMPONENTS})
